c6dd8f9cd38e808f3bd2b8f847d054b442a45b53
Joadoa1992 <jolu0001@stud.kea.dk>
2025-05-03 13:45:53 +0200

    Admin can be on any website now

--- src/main/java/org/example/semester2_eksamensprojekt/controller/AutoRepairController.java
+++ src/main/java/org/example/semester2_eksamensprojekt/controller/AutoRepairController.java
@@ -1,13 +1,23 @@
 package org.example.semester2_eksamensprojekt.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class AutoRepairController {
 
     @GetMapping("/autoRepair")
-    public String mainPage(){
-        return "autoRepair";
+    public String autoRepairPage(@RequestParam("user_role") String user_role, Model model) {
+        //Her tjekker den om url har den rigtig user_role og sender en tilbage til start siden hvis den ikke har. Med en errorMessage
+        if(user_role.equals("admin")) {
+            return "autoRepair";
+        } else if (user_role.equals("mechanic")) {
+            return "autoRepair";
+
+        } else
+            model.addAttribute("errorMessage", "Den rolle passer ikke til den side du prøvet at komme ind på");
+        return "index";
     }
 }
--- src/main/java/org/example/semester2_eksamensprojekt/controller/BusinessDeveloperController.java
+++ src/main/java/org/example/semester2_eksamensprojekt/controller/BusinessDeveloperController.java
@@ -1,7 +1,9 @@
 package org.example.semester2_eksamensprojekt.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
@@ -9,7 +11,15 @@ public class BusinessDeveloperController {
 
 
     @GetMapping("/businessDeveloper")
-    public String mainPage(){
-        return "businessDeveloper";
+    public String businessDeveloperPage(@RequestParam("user_role") String user_role, Model model) {
+        //Her tjekker den om url har den rigtig user_role og sender en tilbage til start siden hvis den ikke har. Med en errorMessage
+        if(user_role.equals("admin")) {
+            return "businessDeveloper";
+        } else if (user_role.equals("business_developer")) {
+            return "businessDeveloper";
+
+        } else
+            model.addAttribute("errorMessage", "Den rolle passer ikke til den side du prøvet at komme ind på");
+        return "index";
     }
 }
