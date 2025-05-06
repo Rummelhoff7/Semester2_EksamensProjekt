122e53c624b75e2812af8eaead142982d47117a7
Joadoa1992 <jolu0001@stud.kea.dk>
2025-05-06 12:33:56 +0200

    Trying to get pull request working

--- src/main/resources/BilabonnementDatabase.sql
+++ src/main/resources/BilabonnementDatabase.sql
@@ -4,6 +4,61 @@ CREATE DATABASE BilabonnementDatabase;
 
 USE BilabonnementDatabase;
 
+
+
+CREATE TABLE leasing (
+                         id INT AUTO_INCREMENT PRIMARY KEY,
+                         car_id INT NOT NULL,
+                         start_date DATE,
+                         end_date DATE,
+                         price DOUBLE,
+                         status BOOLEAN,
+                         customer_info TEXT
+    /*  FOREIGN KEY (car_id)
+    REFERENCES car(id) */
+);
+
+INSERT INTO leasing (car_id, start_date, end_date, price, status, customer_info) VALUES
+                                                                                     (1, '2025-05-02', '2025-09-02',40000.00, false, 'Markus Johansen'),
+                                                                                     (2, '2025-05-02', '2025-10-02',75000.00, false, 'Jakob Hansen'),
+                                                                                     (3, '2025-05-02', '2025-09-01',62000.00, false, 'Martin Gunnersen'),
+                                                                                     (4, '2025-05-02', '2025-10-02',56000.00, true, 'Alexander Frandsen'),
+                                                                                     (5, '2025-05-02', '2025-10-02',60000.00, true, 'Lukas Lucasson'),
+                                                                                     (6, '2025-05-02', '2025-11-01',80000.00, false, 'Sofie Jakobsen'),
+                                                                                     (7, '2025-05-02', '2025-12-24',88000.00, false, 'Amir Zakariasen')
+;
+
+CREATE TABLE damagereport (
+                              id INT AUTO_INCREMENT PRIMARY KEY,
+                              car_id INT NOT NULL,
+                              date DATE
+    /*  FOREIGN KEY (car_id)
+     REFERENCES car(id) */
+);
+
+INSERT INTO damagereport (car_id, date) VALUES
+    (8,'2025-05-02')
+/*  Damagereport skal kun være på biler der er færdige med leasing */
+;
+
+CREATE TABLE damageitem (
+                            id INT AUTO_INCREMENT PRIMARY KEY,
+                            dmg_id INT NOT NULL,
+                            description TEXT,
+                            cost DOUBLE,
+                            FOREIGN KEY (dmg_id)
+                                REFERENCES damagereport(id)
+);
+
+INSERT INTO damageitem (dmg_id, description, cost) VALUES
+    (1, 'Manglende sidespejl', 5000.00)
+;
+
+
+
+
+
+
 CREATE TABLE user (
                       id INT AUTO_INCREMENT PRIMARY KEY,
                       name VARCHAR(100) NOT NULL UNIQUE,
@@ -64,74 +119,4 @@ INSERT INTO cars(id, framenumber, color, brand, model, equipment_level, steel_pr
             (23, 'YV1TS92D521000023', 'Blue', 'Volvo', 'XC60', 4, 210000.00, 300000.00, 135.00, TRUE, 'Leased', 'volvoxc60blue.jpg'),
             (24, 'VF1JZ1J0C567000024', 'White', 'Renault', 'Captur', 1, 90000.00, 170000.00, 120.00, FALSE, 'Available', 'renaultcapturwhite.webp'),
             (25, 'ZFF67NFA4D0200025', 'Black', 'Ferrari', 'Portofino M', 4, 1000000.00, 1700000.00, 270.00, FALSE, 'Leased', 'ferrariblack.webp')
-;
-
-CREATE TABLE leasing (
-                         id INT AUTO_INCREMENT PRIMARY KEY,
-                         car_id INT NOT NULL,
-                         start_date DATE,
-                         end_date DATE,
-                         price DOUBLE,
-                         status BOOLEAN,
-                         customer_info TEXT
-    /*  FOREIGN KEY (car_id)
-    REFERENCES car(id) */
-);
-
-INSERT INTO leasing (car_id, start_date, end_date, price, status, customer_info) VALUES
-      (1, '2025-05-02', '2025-09-02',40000.00, false, 'Markus Johansen'),
-      (2, '2025-05-02', '2025-10-02',75000.00, false, 'Jakob Hansen'),
-      (3, '2025-05-02', '2025-09-01',62000.00, false, 'Martin Gunnersen'),
-      (4, '2025-05-02', '2025-10-02',56000.00, true, 'Alexander Frandsen'),
-      (5, '2025-05-02', '2025-10-02',60000.00, true, 'Lukas Lucasson'),
-      (6, '2025-05-02', '2025-11-01',80000.00, false, 'Sofie Jakobsen'),
-      (7, '2025-05-02', '2025-12-24',88000.00, false, 'Amir Zakariasen')
-;
-
-CREATE TABLE leasing (
-                         id INT AUTO_INCREMENT PRIMARY KEY,
-                         car_id INT NOT NULL,
-                         start_date DATE,
-                         end_date DATE,
-                         price DOUBLE,
-                         status BOOLEAN,
-                         customer_info TEXT
-    /*  FOREIGN KEY (car_id)
-    REFERENCES car(id) */
-);
-
-INSERT INTO leasing (car_id, start_date, end_date, price, status, customer_info) VALUES
-                                                                                     (1, '2025-05-02', '2025-09-02',40000.00, false, 'Markus Johansen'),
-                                                                                     (2, '2025-05-02', '2025-10-02',75000.00, false, 'Jakob Hansen'),
-                                                                                     (3, '2025-05-02', '2025-09-01',62000.00, false, 'Martin Gunnersen'),
-                                                                                     (4, '2025-05-02', '2025-10-02',56000.00, true, 'Alexander Frandsen'),
-                                                                                     (5, '2025-05-02', '2025-10-02',60000.00, true, 'Lukas Lucasson'),
-                                                                                     (6, '2025-05-02', '2025-11-01',80000.00, false, 'Sofie Jakobsen'),
-                                                                                     (7, '2025-05-02', '2025-12-24',88000.00, false, 'Amir Zakariasen')
-;
-
-CREATE TABLE damagereport (
-                              id INT AUTO_INCREMENT PRIMARY KEY,
-                              car_id INT NOT NULL,
-                              date DATE
-    /*  FOREIGN KEY (car_id)
-     REFERENCES car(id) */
-);
-
-INSERT INTO damagereport (car_id, date) VALUES
-    (8,'2025-05-02')
-/*  Damagereport skal kun være på biler der er færdige med leasing */
-;
-
-CREATE TABLE damageitem (
-                            id INT AUTO_INCREMENT PRIMARY KEY,
-                            dmg_id INT NOT NULL,
-                            description TEXT,
-                            cost DOUBLE,
-                            FOREIGN KEY (dmg_id)
-                                REFERENCES damagereport(id)
-);
-
-INSERT INTO damageitem (dmg_id, description, cost) VALUES
-    (1, 'Manglende sidespejl', 5000.00)
 ;
