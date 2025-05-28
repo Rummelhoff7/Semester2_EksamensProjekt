6a48aa35f2e0f09411d8650f7ba3fcea49659b19
Rummelhoff7 <mads.jack.rummelhoff@hotmail.com>
2025-05-28 10:31:07 +0200

    Integration Test kommentarer

--- src/test/java/org/example/semester2_eksamensprojekt/DataRegistrationRepository_IntegrationTest.java
+++ src/test/java/org/example/semester2_eksamensprojekt/DataRegistrationRepository_IntegrationTest.java
@@ -14,38 +14,45 @@ import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 
-//Integration test = test med flere lag, Controller, Repository, database osv.
+// Integration test = test med flere lag, Controller, Repository, database osv.
+// Disse tester samarbejdet mellem databasen og repository.
 @SpringBootTest
 public class DataRegistrationRepository_IntegrationTest {
-
-
     //Ingen Mocks
-
     @Autowired
     private DataRegistrationRepository dataRegistrationRepository;
 
-
+    // Denne test tjekker getAllLeasing() fra DataRegRepository, som vi forventer et happy flow.
     @Test
     @DisplayName("Get all leasings happy flow integration test")
     public void getAllLeasing_HappyFlow_Test() {
         // Assumptions
+            // Test data
         int testCarId = 1;
         LocalDate startDate = LocalDate.now();
+            // Gemmer et Leasing objekt i databasen.
         dataRegistrationRepository.save(new Leasing(testCarId, startDate, startDate.plusMonths(5), 3500.0, true, "Test"));
 
         // Execution
+            // Da vi har gemt et leasing objekt, ved vi at vi har data i databasen.
+            // Så vi tester denne metode, om den returnerer listen af leasings som den skal.
         ArrayList<Leasing> leasings = dataRegistrationRepository.getAllLeasings();
 
         // Validation
+            // Vi tjekker om den returneret listen ikke er tom.
         assertThat(leasings).isNotEmpty();
 
+            // Derefter går vi igennem listen af leasings, og prøver at finde den oprettede leasing.
         boolean foundTestLeasing = false;
         for (Leasing l : leasings) {
+            // Leasingen skal have samme id som den oprettede leasing.
             if (l.getCar_id() == testCarId) {
                 foundTestLeasing = true;
                 break;
             }
         }
+            //Hvis fountTestLeasing er true, så har vi fundet den oprettede leasing.
+            //Hvis false, vil testen fejle.
         assertTrue(foundTestLeasing, "Should find the test leasing in the list");
     }
 
